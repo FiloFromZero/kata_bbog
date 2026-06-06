3b0e36a78ffc63ddc8023259f77d5628ebc4810a
FiloFromZero <montoyamateo138@gmail.com>
2026-06-05 19:04:13 -0500

    Fase 0:Commit Inicial

---
+++ Back-End/src/main/java/com/customers/kata_bbog/KataBbogApplication.java
@@ -0,0 +1,13 @@
+package com.customers.kata_bbog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataBbogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataBbogApplication.class, args);
+	}
+
+}
---
+++ Back-End/src/test/java/com/customers/kata_bbog/KataBbogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.customers.kata_bbog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataBbogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
